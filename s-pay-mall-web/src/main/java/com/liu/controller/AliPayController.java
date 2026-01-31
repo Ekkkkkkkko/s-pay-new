d14f72ac3f38145e3db420ff9ef2eb62d20076ed
Ekko <Ekko.xy.Liu@outlook.com>
2026-01-31 10:41:20 +0800

    2.0 createpay

---
+++ s-pay-mall-web/src/main/java/com/liu/controller/AliPayController.java
@@ -0,0 +1,123 @@
+package com.liu.controller;
+
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.liu.common.constants.Constants;
+import com.liu.common.response.Response;
+import com.liu.controller.DTO.CreatePayRequestDTO;
+import com.liu.domain.res.PayOrderRes;
+import com.liu.service.IOrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Ekko
+ * @description
+ * @create 2026/1/31 10:03
+ */
+@Slf4j
+public class AliPayController {
+    @Value("${alipay.lipay_public_key}")
+    private String alipayPublicKey;
+    @Resource
+    private IOrderService orderService;
+
+    public Response<String> createPayOrder(@RequestBody CreatePayRequestDTO createPayRequestDTO){
+        try{
+            log.info("商品下单，根据商品ID创建支付单开始 userid:{} productId:{}",createPayRequestDTO.getUserId(),createPayRequestDTO.getUserId());
+            String userId = createPayRequestDTO.getUserId();
+            String productId = createPayRequestDTO.getProductId();
+            PayOrderRes payOrderRes = orderService.createOrder(ShopCartReq.builder()
+                    .userId(userId)
+                    .productId(productId)
+                    .build());
+
+            log.info("商品下单，根据商品ID创建支付单完成 userId:{} productId:{} orderId:{}", userId, productId, payOrderRes.getOrderId());
+            return Response.<String>builder()
+                    .code(Constants.ResponseCode.SUCCESS.getCode())
+                    .info(Constants.ResponseCode.SUCCESS.getInfo())
+                    .data(payOrderRes.getPayUrl())
+                    .build();
+        } catch (Exception e) {
+            log.error("商品下单，根据商品ID创建支付单失败 userId:{} productId:{}", createPayRequestDTO.getUserId(), createPayRequestDTO.getUserId(), e);
+            return Response.<String>builder()
+                    .code(Constants.ResponseCode.UN_ERROR.getCode())
+                    .info(Constants.ResponseCode.UN_ERROR.getInfo())
+                    .build();
+        }
+    }
+
+    /**
+     * http://xfg-studio.natapp1.cc/api/v1/alipay/alipay_notify_url
+     */
+    @RequestMapping(value = "alipay_notify_url", method = RequestMethod.POST)
+    public String payNotify(HttpServletRequest request) throws AlipayApiException {
+        log.info("支付回调，消息接收 {}", request.getParameter("trade_status"));
+
+        if (!request.getParameter("trade_status").equals("TRADE_SUCCESS")) {
+            return "false";
+        }
+
+        Map<String, String> params = new HashMap<>();
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (String name : requestParams.keySet()) {
+            params.put(name, request.getParameter(name));
+        }
+
+        String tradeNo = params.get("out_trade_no");
+        String gmtPayment = params.get("gmt_payment");
+        String alipayTradeNo = params.get("trade_no");
+
+        String sign = params.get("sign");
+        String content = AlipaySignature.getSignCheckContentV1(params);
+        boolean checkSignature = AlipaySignature.rsa256CheckContent(content, sign, alipayPublicKey, "UTF-8"); // 验证签名
+        // 支付宝验签
+        if (!checkSignature) {
+            return "false";
+        }
+
+        // 验签通过
+        log.info("支付回调，交易名称: {}", params.get("subject"));
+        log.info("支付回调，交易状态: {}", params.get("trade_status"));
+        log.info("支付回调，支付宝交易凭证号: {}", params.get("trade_no"));
+        log.info("支付回调，商户订单号: {}", params.get("out_trade_no"));
+        log.info("支付回调，交易金额: {}", params.get("total_amount"));
+        log.info("支付回调，买家在支付宝唯一id: {}", params.get("buyer_id"));
+        log.info("支付回调，买家付款时间: {}", params.get("gmt_payment"));
+        log.info("支付回调，买家付款金额: {}", params.get("buyer_pay_amount"));
+        log.info("支付回调，支付回调，更新订单 {}", tradeNo);
+
+        orderService.changeOrderPaySuccess(tradeNo);
+
+        return "success";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
